36ea3f111ec736f7ca3423c9f8b86eed7531b4f4
Ludovico C. <key.well@oulook.com>
2026-03-13 14:45:58 +0000

    feature: add posts with genius layout

--- frontend/keystatic.config.ts
+++ frontend/keystatic.config.ts
@@ -12,7 +12,22 @@ export default config({
       format: { contentField: 'content' },
       schema: {
         title: fields.slug({ name: { label: 'Title' } }),
-        content: fields.markdoc({ label: 'Content' }),
+        artist: fields.text({ label: 'Artist' }),
+        album: fields.text({ label: 'Album' }),
+        content: fields.markdoc({ label: 'Intro' }),
+        lyrics: fields.array(
+          fields.object({
+            line: fields.text({ label: 'Lyric Line' }),
+            annotation: fields.text({ 
+              label: 'Annotation/Explanation',
+              multiline: true,
+            }),
+          }),
+          {
+            label: 'Lyrics',
+            itemLabel: (props) => props.fields.line.value || 'New line',
+          }
+        ),
       },
     }),
   },
